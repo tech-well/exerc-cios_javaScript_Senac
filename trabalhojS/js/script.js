0ed787f674216d58135fa1925b4780556b42c452
wellington <wellingtoncarlos.tech@gmail.com>
2024-08-28 16:03:18 -0300

    exercício data e hora

--- trabalhojS/js/script.js
+++ trabalhojS/js/script.js
@@ -1,44 +1,65 @@
+
+let pularLinha = ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+
 let meuNome = 'wellington carlos';
 console.log(meuNome)
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+console.log(pularLinha)
 let programar = true;
 console.log(programar)
 
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+console.log(pularLinha)
 
 let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
 console.log(n[1] + (n[2] * n[3]) - n[4]);
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+console.log(pularLinha)
+
 console.log(n[1] * (n[2] + n[3]) - n[4] / n[4]);
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+console.log(pularLinha)
+
 console.log(([(n[9] - n[3]) / n[1]] * n[2]) + n[0])
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+console.log(pularLinha)
+
 console.log(n[2] * n[3] / n[1] + n[0] - n[4])
 
 let frase = 'boa'
 let frase2 = 'ruim'
 let frase3 = 'notícia'
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+console.log(pularLinha)
+
 console.log(`Tenho uma ${frase3} ${frase} e uma ${frase2} pra te dar
 A ${frase2} é que não tem ${frase3} ${frase}
 E a ${frase} é que não tem ${frase3} ${frase2}.`)
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+console.log(pularLinha)
+
 let data = new Date()
+
 console.log(data)
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+console.log(pularLinha)
+
 let dia = new Date()
 let d = dia.getDate()
 console.log(d)
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+console.log(pularLinha)
+
 let mes = new Date()
 let m = mes.getMonth()+1
 console.log(m)
-console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+console.log(pularLinha)
 
 let ano = new Date()
 a = ano.getFullYear()
 console.log(a)
+
+console.log(`A data e hora de hoje é ${data}`)
 
 
