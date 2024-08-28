9977de9dc4c86ab8876e1a3f1a1a790e951824ec
WELLINGTON CARLOS DA SILVA LEITE <wellington.csleite@senacsp.edu.br>
2024-08-28 11:22:45 -0300

    exercícios javaScript senac

---
+++ trabalhojS/js/script.js
@@ -0,0 +1,44 @@
+let meuNome = 'wellington carlos';
+console.log(meuNome)
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+let programar = true;
+console.log(programar)
+
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+console.log(n[1] + (n[2] * n[3]) - n[4]);
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+console.log(n[1] * (n[2] + n[3]) - n[4] / n[4]);
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+console.log(([(n[9] - n[3]) / n[1]] * n[2]) + n[0])
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+console.log(n[2] * n[3] / n[1] + n[0] - n[4])
+
+let frase = 'boa'
+let frase2 = 'ruim'
+let frase3 = 'notícia'
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+console.log(`Tenho uma ${frase3} ${frase} e uma ${frase2} pra te dar
+A ${frase2} é que não tem ${frase3} ${frase}
+E a ${frase} é que não tem ${frase3} ${frase2}.`)
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+let data = new Date()
+console.log(data)
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+let dia = new Date()
+let d = dia.getDate()
+console.log(d)
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+let mes = new Date()
+let m = mes.getMonth()+1
+console.log(m)
+console.log("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+let ano = new Date()
+a = ano.getFullYear()
+console.log(a)
+
+
