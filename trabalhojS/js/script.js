bcefa9ab6fefc93194f324169e5cbc97c3374b88
wellington <wellingtoncarlos.tech@gmail.com>
2024-08-28 22:46:55 -0300

    fim exercício javaScript senac

--- trabalhojS/js/script.js
+++ trabalhojS/js/script.js
@@ -1,13 +1,24 @@
 
 let pularLinha = ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-
-let meuNome = 'wellington carlos';
+//  Crie uma variável para armazenar seu nome completo
+let meuNome = 'wellington carlos da silva leite';
 console.log(meuNome)
 console.log(pularLinha)
+
+// Crie uma variável para armazenar se você gosta de programar (true ou false)
 let programar = true;
 console.log(programar)
-
 console.log(pularLinha)
+
+/* Dadas as operações matemáticas abaixo, calcule-as utilizando Javascript, para cada número utilizar uma variável
+
+//2 + (3 x 4) - 5
+
+//2 x (3 + 4) - 5 ÷ 5
+
+//{[(10 - 4) / 2] * 3} + 1
+
+//3 * 4 / 2 + 1 - 5*/
 
 let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -25,7 +36,9 @@ console.log(([(n[9] - n[3]) / n[1]] * n[2]) + n[0])
 console.log(pularLinha)
 
 console.log(n[2] * n[3] / n[1] + n[0] - n[4])
+console.log(pularLinha)
 
+// Crie um poema utilizando interpolação para inserir variáveis em diferentes linhas
 let frase = 'boa'
 let frase2 = 'ruim'
 let frase3 = 'notícia'
@@ -38,6 +51,7 @@ E a ${frase} é que não tem ${frase3} ${frase2}.`)
 
 console.log(pularLinha)
 
+// Crie um programa que exiba a data atual formatada utilizando interpolação
 let data = new Date()
 
 console.log(data)
@@ -64,7 +78,7 @@ console.log(`A data e hora de hoje é ${data}`)
 
 console.log(pularLinha)
 
-
+// Crie um programa que calcule a área de um retângulo, solicitando a base e a altura ao usuário. Exiba o resultado utilizando interpolação
 
 // let nu = parseInt(prompt('digite a base'))
 // let nu2 = parseInt(prompt('digite a altura'))
@@ -73,6 +87,7 @@ console.log(pularLinha)
 
 // console.log(`a area desse retângulo é ${nu3}`)
 
+// Crie uma condicional para verificar se o caractere é uma vogal
 function vogal(letra) {
     
     letra = letra.toLowerCase();
@@ -90,6 +105,8 @@ function vogal(letra) {
   console.log(vogal('b')); 
 console.log(pularLinha)
 
+
+//   /Crie uma condicional para verificar se o número é par ou é impar
   function ParImpar(n1){
 
     if(n1%2==1){
@@ -107,6 +124,8 @@ console.log(pularLinha)
 
  console.log(pularLinha)
 
+//  Crie uma condicional if else if else para verificar se um triângulo é equilátero, isósceles ou escaleno:
+
  let t1 = 5;
 let t2 = 5;
 let t3 = 5;
@@ -119,6 +138,8 @@ if (t1 === t2 && t2 === t3) {
   console.log("O triângulo é escaleno.")}
 
   console.log(pularLinha)
+
+//   Calcule a média do aluno e verifique se ele foi aprovado
 
   function Media(p1,p2,p3){
      let resultado = null
@@ -135,11 +156,18 @@ if (t1 === t2 && t2 === t3) {
 
   console.log(pularLinha)
 
+//   Crie uma variável para armazenar um array com seus hobbies
+
   let hobbies = ['tocar viola','ouvir moda de viola','assistir séries','passear']
 
   console.log(hobbies)
   
   console.log(pularLinha)
+
+/*Crie um array chamado frutas com os nomes de 5 frutas diferentes.
+Acesse e imprima o segundo elemento do array.
+Adicione mais duas frutas ao final do array.
+Remova a primeira fruta do array.*/
 
   const frutas = ['pêra', 'maçã','pitomba','jaca','siriguela']
 
@@ -152,6 +180,15 @@ if (t1 === t2 && t2 === t3) {
   frutas.shift()
 
   console.log(frutas)
+
+  /* 
+Crie uma variável e atribua um valor de qualquer tipo (número, string, booleano).
+Utilize o operador typeof para verificar o tipo da variável.
+Imprima o tipo da variável no console.
+*/
+  let tipo = 'j'
+
+  console.log(typeof(tipo))
 
 
  
